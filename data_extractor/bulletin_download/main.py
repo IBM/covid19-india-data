f18ead2a295feef6c321d9d27112e9974d0dbf97
Mayank-Agarwal <mayank.agarwal@ibm.com>
2021-08-23 18:21:45 -0400

    add TN bulletin download script

--- data_extractor/bulletin_download/main.py
+++ data_extractor/bulletin_download/main.py
@@ -3,12 +3,13 @@ import sys
 from bulletin_download.states.DL import Delhi
 from bulletin_download.states.WB import WestBengal
 from bulletin_download.states.TG import Telangana
+from bulletin_download.states.TN import TamilNadu
 
 
 def run(basedir):
 
     state_downloaders = [
-        Telangana, WestBengal, Delhi
+        Telangana, WestBengal, Delhi, TamilNadu
     ]
 
     for state in state_downloaders:
---
+++ data_extractor/bulletin_download/states/TN.py
@@ -0,0 +1,57 @@
+from os import link
+from .bulletin import Bulletin
+from bs4 import BeautifulSoup
+
+import re
+
+
+class TamilNadu(Bulletin):
+
+    def __init__(self, basedir):
+
+        statename = 'TN'
+        super().__init__(basedir, statename)
+
+        self.bulletin_url = 'https://stopcorona.tn.gov.in/daily-bulletin/'
+        self.archive_bulletin_url = 'https://stopcorona.tn.gov.in/archive/'
+        self.date_regex = re.compile(r'(\d{2})[\.-](\d{2})[\.-](\d{4})')
+
+    def get_bulletin_links(self, url):
+
+        html = self.get_url_html(url)
+        html_parsed = BeautifulSoup(html, 'html.parser')
+        bulletin_anchors = [
+            anchor for anchor in html_parsed.find_all('a')
+            if 'bulletin' in anchor.text.lower()
+        ]
+
+        link_dict = {}
+
+        for anchor in bulletin_anchors:
+            
+            try:
+                anchor_str = anchor.text.lower().strip()
+                anchor_href = anchor['href']
+
+                match = self.date_regex.search(anchor_str)
+                day, month, year = match.groups()
+                datestr = f'{year}-{month}-{day}'
+                datestr = self.get_date_str(datestr)
+            except:
+                pass
+            else:
+                link_dict[datestr] = anchor_href
+
+        return link_dict
+
+    def run(self):
+
+        print(f'\t Downloading Tamil Nadu bulletins')
+
+        linkdict1 = self.get_bulletin_links(self.bulletin_url)
+        linkdict2 = self.get_bulletin_links(self.archive_bulletin_url)
+        
+        bulletin_links = linkdict1.update(linkdict2)
+        
+        self.download_bulletins(bulletin_links)
+        self._save_state_()
