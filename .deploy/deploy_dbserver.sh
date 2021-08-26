7b74f76b7a6a9758b51d6ec0a8ec7dffbb280670
Mayank-Agarwal <mayank.agarwal@ibm.com>
2021-08-26 13:04:16 -0400

    fix build configuration

--- .deploy/deploy_dbserver.sh
+++ .deploy/deploy_dbserver.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-IBM_CLOUD_DBSERVER_SPACE="covid19-data-india-storage"
+IBM_CLOUD_DBSERVER_SPACE="covid-19-data-india-storage"
 
 ibmcloud target -s ${IBM_CLOUD_DBSERVER_SPACE} -o ${IBM_CLOUD_ORG}
-cd ${TRAVIS_BUILD_DIR}/covid19-india-data/serve_db
+cd ${TRAVIS_BUILD_DIR}/serve_db
 
 # Download latest DB from the source folder
 curl -L -o "./covid-india.db" "https://www.dropbox.com/s/hbe04q6vtzapdam/covid-india.db?dl=1"
--- .deploy/deploy_frontend.sh
+++ .deploy/deploy_frontend.sh
@@ -3,6 +3,7 @@
 IBM_CLOUD_FRONTEND_SPACE="covid-19-data-india"
 
 ibmcloud target -s ${IBM_CLOUD_FRONTEND_SPACE} -o ${IBM_CLOUD_ORG}
-cd ${TRAVIS_BUILD_DIR}/covid19-india-data/frontend
+
+cd ${TRAVIS_BUILD_DIR}/frontend
 yarn build
 ibmcloud cf push
