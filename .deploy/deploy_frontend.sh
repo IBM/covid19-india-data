d850fb4bd864b2258c6949fb912f66ec92f13de3
Mayank-Agarwal <mayank.agarwal@ibm.com>
2021-08-26 13:13:52 -0400

    deployment bug fix

--- .deploy/deploy_frontend.sh
+++ .deploy/deploy_frontend.sh
@@ -5,5 +5,6 @@ IBM_CLOUD_FRONTEND_SPACE="covid-19-data-india"
 ibmcloud target -s ${IBM_CLOUD_FRONTEND_SPACE} -o ${IBM_CLOUD_ORG}
 
 cd ${TRAVIS_BUILD_DIR}/frontend
+yarn
 yarn build
 ibmcloud cf push
