08c52264ab98a44a5ae8e38bfc124444756ba1e7
Sushovan De <sushovan@google.com>
2021-11-01 18:27:27 +0530

    undo autoformat of data_extractor/run.py

--- data_extractor/run.py
+++ data_extractor/run.py
@@ -15,12 +15,9 @@ STATES = ['MH', 'HR', 'TG', 'WB', 'DL', 'KA']
 def get_parser():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--datadir', type=str, required=True,
-                        help='Data directory path to store bulletins and database')
-    parser.add_argument('--run_only', type=str, required=False, default=None,
-                        help='Comma-separated values of states to run data extraction for')
-    parser.add_argument('--force_run_states', type=str, required=False, default=None,
-                        help='Comma-separated values of states to force re-run data extraction procedure for')
+    parser.add_argument('--datadir', type=str, required=True, help='Data directory path to store bulletins and database')
+    parser.add_argument('--run_only', type=str, required=False, default=None, help='Comma-separated values of states to run data extraction for')
+    parser.add_argument('--force_run_states', type=str, required=False, default=None, help='Comma-separated values of states to force re-run data extraction procedure for')
 
     return parser
 
@@ -38,12 +35,10 @@ def run(args):
     # Get list of states to force a re-run
     force_rerun_states = []
     if args.force_run_states is not None:
-        force_rerun_states = [x.strip()
-                              for x in args.force_run_states.split(',')]
-
+        force_rerun_states = [x.strip() for x in args.force_run_states.split(',')]
+    
     # Download bulletins
-    bulletin_links = bulletin_downloader.run(
-        args.datadir, state_to_execute=states_to_execute)
+    bulletin_links = bulletin_downloader.run(args.datadir, state_to_execute=states_to_execute)
 
     # Setup tables
     db_obj = DBMain(args.datadir)
@@ -59,23 +54,21 @@ def run(args):
 
         state_pbar.set_description(f'State: {state}')
 
-        metadata_path = os.path.join(
-            args.datadir, 'metadata', 'bulletins', f'{state}.json')
+        metadata_path = os.path.join(args.datadir, 'metadata', 'bulletins', f'{state}.json')
         with open(metadata_path, 'r') as f:
             data = json.load(f)
 
         if PROCESSED_DATES_STR not in data:
             data[PROCESSED_DATES_STR] = []
 
-        date_pbar = tqdm(data[BULLETIN_PATH_STR].items(),
-                         desc="Dates", leave=False)
+        date_pbar = tqdm(data[BULLETIN_PATH_STR].items(), desc="Dates", leave=False)
         for date, fpath in date_pbar:
 
             date_pbar.set_description(f'Date: {date}')
 
             if state not in force_rerun_states and date in data[PROCESSED_DATES_STR]:
                 continue
-
+            
             try:
                 stateinfo = extractor_main.extract_info(state, date, fpath)
                 db_obj.insert_for_state(state, stateinfo)
@@ -83,7 +76,7 @@ def run(args):
                 print(f'Error in parsing date: {date}. Error: {err}')
             else:
                 data[PROCESSED_DATES_STR].append(date)
-
+        
         with open(metadata_path, 'w') as f:
             json.dump(data, f)
 
