83dd64c8f7615d7b8a0427180ce7160577e1dac2
Sushovan De <sushovan@google.com>
2021-11-01 16:53:00 +0530

    typo

--- data_extractor/local_extractor/main.py
+++ data_extractor/local_extractor/main.py
@@ -2,7 +2,7 @@ from local_extractor.states.DL import DelhiExtractor
 from local_extractor.states.WB import WestBengalExtractor
 from local_extractor.states.TG import TelanganaExtractor
 from local_extractor.states.HR import HaryanaExtractor
-from local_extractor.states.HR import KarnatakaExtractor
+from local_extractor.states.KA import KarnatakaExtractor
 
 
 STATE_LIST = {
