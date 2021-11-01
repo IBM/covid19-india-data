c027ab7af91925905f6d803b48531b9ea9640989
Sushovan De <sushovan@google.com>
2021-11-01 18:01:12 +0530

    fixed district extraction, hopefully

--- data_extractor/local_extractor/states/KA.py
+++ data_extractor/local_extractor/states/KA.py
@@ -1,23 +1,19 @@
 import locale
-locale.setlocale( locale.LC_ALL, 'en_US.UTF-8' )
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
-import re
-import pandas as pd
-import camelot
-import pdfminer
-
-from pdfminer.high_level import extract_pages
-from pprint import pprint
 
 try:
     from local_extractor.utils import common_utils
 except ImportError:
-    import sys, os, pathlib
+    import sys
+    import os
+    import pathlib
     path = pathlib.Path(__file__).absolute().parents[2]
     path = os.path.join(path, 'local_extractor')
     if path not in sys.path:
         sys.path.insert(0, path)
     from utils import common_utils
+
 
 class KarnatakaExtractor(object):
     def __init__(self, date, report_fpath):
@@ -41,7 +37,7 @@ class KarnatakaExtractor(object):
         detected_value = None
         for it in series:
             for k, v in term_before_value.items():
-                if (re.search(k, it, re.IGNORECASE)):
+                if k in it:
                     detected_term = v
                 raw_string = it.strip()
                 if raw_string.isdigit():
@@ -67,26 +63,45 @@ class KarnatakaExtractor(object):
 
         return df_dict
 
-    def extract_district_case_information(self, table):
+    def find_table_with_text(self, tables, text_to_find):
+        for t in tables:
+            for row in t.data:
+                for col in row:
+                    if (text_to_find in col):
+                        return t
+        return None
+
+    def extract_district_case_information(self, tables):
+        if tables is None:
+            return None
+
+        table = self.find_table_with_text(tables, 'Districtwise Abstract')
         if table is None:
             return None
-        d = table.df
-        # The first 2 rows are blank. The third row is the table title. The fourth row
-        # has the column headers. The last two rows have the totals and some footnote.
-        d = d.iloc[4:(len(d)-2)]
+
         result = []
-        for i, row in d.iterrows():
+        for i, row in table.df.iterrows():
+            squished_text = ''.join(row).strip()
+            # The first 2 rows are blank. The third row is the table title. The fourth row
+            # has the column headers. The last two rows have the totals and maybe some footnote.
+            if (
+                    len(squished_text) < 2 or
+                    squished_text.startswith('Districtwise Abstract') or
+                    squished_text.startswith('Total') or
+                    squished_text.startswith('Sl.')):
+                continue
+
             tmp = {
                 'date': self.date,
-                'district': row[2],
-                'cases_new': row[3],
-                'cases_total': row[4],
-                'discharged_new': row[5],
-                'discharged_total': row[6],
-                'active_cases_total': row[7],
-                'deaths_new': row[8],
-                'deaths_total': row[9],
-                'non_covid_deaths': row[10]
+                'district': row[1],
+                'cases_new': row[2],
+                'cases_total': row[3],
+                'discharged_new': row[4],
+                'discharged_total': row[5],
+                'active_cases_total': row[6],
+                'deaths_new': row[7],
+                'deaths_total': row[8],
+                'non_covid_deaths': row[9]
             }
             result.append(tmp)
         return result
@@ -94,23 +109,24 @@ class KarnatakaExtractor(object):
     def extract(self):
         n = common_utils.n_pages_in_pdf(self.report_fpath)
 
-        # karnataka has one table per page, and each table inside it has 
+        # karnataka has one table per page, and each table inside it has
         # a complex system of text and tables embedded in it.
 
         # First, we get the overall case numbers from the first page.
         # This has to be done without smart boundary detection.
-        tables_page0 = common_utils.get_tables_from_pdf(library='camelot', 
-            pdf_fpath=self.report_fpath, 
-            smart_boundary_detection=False,
-            pages=[1])
+        tables_page0 = common_utils.get_tables_from_pdf(library='camelot',
+                                                        pdf_fpath=self.report_fpath,
+                                                        smart_boundary_detection=False,
+                                                        pages=[1])
         case_info = self.extract_case_info(tables_page0[0])
 
         # Then, we get the district-wise numbers. This needs smart boundary detection.
-        tables_page4 = common_utils.get_tables_from_pdf(library='camelot', 
-            pdf_fpath=self.report_fpath, 
-            smart_boundary_detection=True,
-            pages=[4])
-        districtwise_info = self.extract_district_case_information(tables_page4[1])
+        tables_page4 = common_utils.get_tables_from_pdf(library='camelot',
+                                                        pdf_fpath=self.report_fpath,
+                                                        smart_boundary_detection=True,
+                                                        pages=[4])
+        districtwise_info = self.extract_district_case_information(
+            tables_page4)
 
         result = {
             'case_info': case_info,
@@ -120,9 +136,10 @@ class KarnatakaExtractor(object):
         return result
 
 
-
 if __name__ == '__main__':
+    import os
     date = '08-oct-2021'
-    path = '/home/sushovan/covdata/bulletins/KA/KA-Bulletin-2021-10-28.pdf'
+    path = os.getenv("HOME") + \
+        '/covlocal/bulletins/KA/KA-Bulletin-2021-10-12.pdf'
     obj = KarnatakaExtractor(date, path)
     print(obj.extract())
