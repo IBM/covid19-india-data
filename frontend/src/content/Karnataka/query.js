19c1a2aa3ad4c8f2cd6b8ec73208ae9043c82916
Mayank Agarwal <MayankAgarwal@users.noreply.github.com>
2021-11-05 15:30:20 -0400

    add KA dashboard and visualization queries (#80)
    * Downloaders for Karnataka
    
    * KA db table configs
    
    * Karnataka data extractor
    
    * quickfix: import error for karnataka
    
    * Added Karnataka to the extractor main
    
    * typo
    
    * typo appears again
    
    * fixed district extraction, hopefully
    
    * undo autoformat of data_extractor/run.py
    
    * footnotes start with an asterisk
    
    * deleting spurious print statement
    
    * key name mismatch for case info table
    
    * feat: add KA frontend
    
    * update KA procedure
    
    * feat: add KA dashboard and visualization queries
    
    Co-authored-by: Sushovan De <sushovan@google.com>
    Co-authored-by: Mayank Agarwal <mayank.agarwal@ibm.com>

--- frontend/src/content/Karnataka/query.js
+++ frontend/src/content/Karnataka/query.js
@@ -1,6 +1,37 @@
 // import React from 'react';
 // import { Link } from 'carbon-components-react';
 
-const QUERIES = [];
+const QUERIES = [
+  {
+    subject: 'Daily new confirmed cases',
+    description: <></>,
+    query:
+      'SELECT date, cases_new from KA_case_info WHERE cases_new is NOT NULL ORDER BY date ASC',
+  },
+  {
+    subject: 'Number of active cases',
+    description: <></>,
+    query:
+      'SELECT date, cases_active from KA_case_info WHERE cases_active is NOT NULL ORDER BY date ASC',
+  },
+  {
+    subject: 'Daily new discharges',
+    description: <></>,
+    query:
+      'SELECT date, discharged_new from KA_case_info WHERE discharged_new is NOT NULL ORDER BY date ASC',
+  },
+  {
+    subject: 'Daily new fatalities',
+    description: <></>,
+    query:
+      'SELECT date, deaths_new from KA_case_info WHERE deaths_new is NOT NULL ORDER BY date ASC',
+  },
+  {
+    subject: 'Test positivity rate',
+    description: <></>,
+    query:
+      'SELECT date, positivity_rate_percent FROM KA_case_info WHERE positivity_rate_percent is NOT NULL ORDER BY date ASC',
+  },
+];
 
 export { QUERIES };
