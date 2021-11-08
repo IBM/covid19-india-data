f4d51bee1665d83530774ca3f9d8fd60d421b66d
Mayank Agarwal <MayankAgarwal@users.noreply.github.com>
2021-11-08 02:49:28 -0500

    add UK queries (#82)
    Co-authored-by: Mayank-Agarwal <mayank.agarwal@ibm.com>

--- frontend/src/content/Uttarakhand/query.js
+++ frontend/src/content/Uttarakhand/query.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import { Link } from 'carbon-components-react';
 
-const QUERIES = [];
+const QUERIES = [
+    {
+        subject: 'Daily new cases',
+        description: <></>,
+        query: 'SELECT date, cases_new FROM UK_case_info WHERE cases_new is NOT NULL ORDER BY date ASC'
+    },
+    {
+        subject: 'Number of active cases',
+        description: <></>,
+        query: 'SELECT date, active_cases FROM UK_case_info WHERE active_cases is NOT NULL ORDER BY date ASC'
+    },
+    {
+        subject: 'Daily tests',
+        description: <></>,
+        query: 'SELECT date, tests_today FROM UK_case_info WHERE tests_today is NOT NULL ORDER BY date ASC'
+    }
+];
 
 export { QUERIES };
