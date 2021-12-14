22ec8627fca21ad2738d5857577bee956a79e6fe
Mayank Agarwal <mayank.agarwal@ibm.com>
2021-12-14 18:01:38 -0500

    feat: add TN queries

--- frontend/src/content/TamilNadu/query.js
+++ frontend/src/content/TamilNadu/query.js
@@ -1,6 +1,25 @@
 // import React from 'react';
 // import { Link } from 'carbon-components-react';
 
-const QUERIES = [];
+const QUERIES = [
+  {
+    subject: 'Daily new confirmed cases',
+    description: null,
+    query:
+      'select date, positive_tested_cases from TN_case_info WHERE positive_tested_cases is NOT NULL ORDER BY date',
+  },
+  {
+    subject: 'Number of active cases',
+    description: null,
+    query:
+      'select date, active_cases_today from TN_case_info WHERE active_cases_today is NOT NULL ORDER BY date',
+  },
+  {
+    subject: 'Daily new fatalities',
+    description: null,
+    query:
+      'select date, deaths_today from TN_case_info WHERE deaths_today is NOT NULL ORDER BY date',
+  },
+];
 
 export { QUERIES };
